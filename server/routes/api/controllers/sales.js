6baa5394ff869107ee61f9763ee0011412da24b9
Patrick Odey <patrickjoseph2121@gmail.com>
2021-10-11 01:43:53 -0700

    Added Sales for the month day and total

--- server/routes/api/controllers/sales.js
+++ server/routes/api/controllers/sales.js
@@ -32,7 +32,24 @@ exports.getAllSales = (req, res) => {
 exports.getSingleSale = (req, res) => {
     Sale.find({vendorId: req.params.vendorId})
     .then(sales => {
-        return res.status(200).json(sales)
+        const todaySales = sales.filter(sale => {
+            const today = new Date();
+            const saleDate = new Date(sale.createdAt);
+            return saleDate.getDate() == today.getDate() && saleDate.getMonth() + 1 == today.getMonth() + 1 &&
+            saleDate.getFullYear() == today.getFullYear()
+        })
+        const monthlySales = sales.filter(sale => {
+            const today = new Date();
+            const saleDate = new Date(sale.createdAt);
+            return saleDate.getMonth() + 1 == today.getMonth() + 1 &&
+            saleDate.getFullYear() == today.getFullYear()
+        })
+        return res.status(200).json({
+            sales,
+            totalSales: sales.length,
+            salesForTheDay: todaySales.length,
+            salesForTheMonth: monthlySales.length
+        })
     })
     .catch(err => {
         return res.status(500).json({err})
